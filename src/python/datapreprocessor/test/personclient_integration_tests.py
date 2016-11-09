b44e8072a2617da400ea4e9264fbd7b227b9d053
Jo Osborne <jo.osborne@footballradar.com>
2016-11-09 22:32:38 +0000

    add setup and tear down methods so data is always deleted even if test fails

--- src/python/datapreprocessor/test/personclient_integration_tests.py
+++ src/python/datapreprocessor/test/personclient_integration_tests.py
@@ -7,23 +7,30 @@ from settings import MEETINGS_API_BASE_URL
 @unittest.skipUnless(INTEGRATION_TESTS, 'api integration tests')
 class PersonClientTest(unittest.TestCase):
 
+    def setUp(self):
+        self.client = PersonClient(MEETINGS_API_BASE_URL)
+
+    def tearDown(self):
+        people = self.client.get_all()
+        for person in people:
+            self.client.delete(person['attributes']['name'])
+
     def test_person_lifecycle(self):
         """This test verifies that a person can be created, retrieved and deleted"""
         person_name = "Freddy the Frog"
-        client = PersonClient(MEETINGS_API_BASE_URL)
 
         # Initial Retrieve (when person does not exist)
-        no_person = client.get_by_name(person_name)
+        no_person = self.client.get_by_name(person_name)
         self.assertEqual(None, no_person)
 
         # Create person
-        create_response = client.add(person_name)
+        create_response = self.client.add(person_name)
         self.assertEqual(requests.codes.created, create_response.status_code)
 
         # Retrieve person
-        person = client.get_by_name(person_name)
+        person = self.client.get_by_name(person_name)
         self.assertEqual(person_name, person['attributes']['name'])
 
         # Delete person
-        delete_response = client.delete(person_name)
+        delete_response = self.client.delete(person_name)
         self.assertEqual(requests.codes.no_content, delete_response.status_code)
